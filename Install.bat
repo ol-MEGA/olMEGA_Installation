c2927110fc0034d386ea558462889ff274fca07c
UlrikKowalk <ulrik.kowalk@jade-hs.de>
2021-07-27 14:28:32 +0200

    New version

--- Install.bat
+++ Install.bat
@@ -1,2 +1,2 @@
-adb install -r data/app-release_2.0beta31.apk
-adb shell dpm set-device-owner com.fragtest.android.pa/.AdminReceiver
+adb install -r data/app-release_2.0beta34.apk
+adb shell dpm set-device-owner com.iha.olmega_mobilesoftware_v2/.AdminReceiver
